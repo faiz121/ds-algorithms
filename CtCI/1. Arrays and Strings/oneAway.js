b225192fd349f00be3e0efced4580f9ac421573f
Faiz Mohammed <faizmohammed178@gmail.com>
2020-10-04 15:17:52 -0700

    Update oneAway.js

--- CtCI/1. Arrays and Strings/oneAway.js
+++ CtCI/1. Arrays and Strings/oneAway.js
@@ -1,16 +1,23 @@
-var palindromPerm = (str1, str2) => {
-  const occurencesSet = new Set();
-  for(var i=0; i<str1.length; i++) {
-    const char = str1[i];
-    occurencesSet.add(char);
-  }
-  for(var i=0; i<str2.length; i++) {
-    const char = str2[i];
-    if(occurencesSet.has(char)) {
-      occurencesSet.delete(char);
+var palindromPerm = (left, right) => {
+  if (Math.abs(left.length - right.length) > 1) return false;
+  let difference = 0;
+  let indexLeft = 0;
+  let indexRight = 0;
+  while (difference < 2 && indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] !== right[indexRight]) {
+      difference++;
+      // Character inserted in left string, or character removed in right string
+      if (left[indexLeft + 1] === right[indexRight]) indexLeft++;
+      // Character inserted in right string, or character removed in left string
+      else if (left[indexLeft] === right[indexRight + 1]) indexRight++;
     }
+    indexLeft++;
+    indexRight++;
   }
-  return occurencesSet.size === 1;
+  // if any remaining characters in either left or right
+  difference +=  left.length - indexLeft;
+  difference +=  right.length - indexRight; 
+  return difference < 2;
 }
 
 palindromPerm("pale", "ple") // true
